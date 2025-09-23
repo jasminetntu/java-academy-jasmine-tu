0a7cdbccf18a216a6359cd4f9f36b9fa903f48fd
Jasmine <jasminetntu@gmail.com>
2025-09-23 15:36:27 -0700

    edit README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# java-academy-jasmine-tu
+# Pluralsight Java Academy
+> LC 1 Bay Bytes, Class 6 | Professor Remsey Mailjard
+
+Jasmine Tu's workbook and class exercises.
+
--- workbook1c/shoppinglist/src/main/java/com/pluralsight/ShoppingList.java
+++ workbook1c/shoppinglist/src/main/java/com/pluralsight/ShoppingList.java
@@ -1,7 +1,5 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ShoppingList {
     public static void main(String[] args) {
         String[] list = {"apples", "bananas", "watermelon", "rice", "meat",
