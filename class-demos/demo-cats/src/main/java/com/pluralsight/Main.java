79a562c55c31a642763501f362848bc54b012007
Jasmine <jasminetntu@gmail.com>
2025-09-24 13:23:00 -0700

    class demo code

--- class-demos/demo-cats/src/main/java/com/pluralsight/Main.java
+++ class-demos/demo-cats/src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,48 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sout is awesome!");
+
+        //primitive variables
+        boolean alreadyAte = false;
+        boolean canCook = false;
+
+        //reference variables
+        String firstName = "Jasmine";
+        String lastName = "Tu";
+        String fullName = firstName + " " + lastName;
+        String favColor = "green";
+
+        String favArtist = "Mitski";
+        System.out.println(fullName + "'s favorite artist is " + favArtist);
+        favArtist = "CAS";
+        System.out.println(fullName + "'s new favorite artist is " + favArtist);
+
+        if (alreadyAte) {
+            System.out.println("Nice! " + firstName + " has energy for the day.");
+        }
+        else if (!alreadyAte && canCook){
+            System.out.println(firstName + " is hungry and can cook.");
+        }
+        else if (!alreadyAte && !canCook) {
+            System.out.println(firstName + " is so hungry, but she can't cook. She's cooked.");
+        }
+
+        //exercise - mood & breakfast checker
+        boolean isHappy = true;
+        boolean hadBreakfast = false;
+
+        if (isHappy && hadBreakfast) {
+            System.out.println("Yay, nice job!");
+        }
+        else if (isHappy && !hadBreakfast) {
+            System.out.println("You might be hungry...");
+        }
+        else if (!isHappy && hadBreakfast) {
+            System.out.println("Was your breakfast bad? :(");
+        }
+        else {
+            System.out.println("You're hangry. Do something quick!");
+        }
+
     }
 }
