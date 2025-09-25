07b3e630d046e59f3a301a4552f2194c3cea0e74
Jasmine <jasminetntu@gmail.com>
2025-09-25 13:28:18 -0700

    create Hello.java

---
+++ workbook1d/MethodPractice/src/main/java/com/pluralsight/Hello.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Hello {
+    public static void main(String[] args) {
+        sayHello();
+        sayGoodbye();
+        sayGoodMorning();
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello! (^-^)/");
+    }
+
+    public static void sayGoodbye() {
+        System.out.println("Goodbye! :p");
+    }
+
+    public static void sayGoodMorning() {
+        System.out.println("Good Morning! ;)");
+    }
+}
