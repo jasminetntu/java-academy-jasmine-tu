5aed897771d87e0006e1b6fdca6b79235ac1c8a1
Jasmine <jasminetntu@gmail.com>
2025-09-25 10:49:35 -0700

    added and completed BasicCalculator.java

---
+++ workbook1d/BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
@@ -0,0 +1,44 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        //get number inputs
+        System.out.print("Enter the first number: ");
+        double num1 = scnr.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scnr.nextDouble();
+
+        //print options
+        System.out.print("""
+                \nPossible calculations:
+                    (A)dd
+                    (S)ubtract
+                    (M)ultiply
+                    (D)ivide
+                Please select an option (corresponding letter):\s""");
+        String choice = scnr.next();
+
+        //perform calculation & print
+        System.out.println();
+
+        if (choice.equalsIgnoreCase("a")) {
+            System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        }
+        else if (choice.equalsIgnoreCase("s")) {
+            System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        }
+        else if (choice.equalsIgnoreCase("m")) {
+            System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        }
+        else if (choice.equalsIgnoreCase("d")) {
+            System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        }
+        else {
+            System.out.println("Invalid choice");
+        }
+    }
+}
