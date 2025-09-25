bc3e87b8f36058296a0d211c282363892f6b1af8
Jasmine <jasminetntu@gmail.com>
2025-09-25 11:37:15 -0700

    part 2 of sandwich shop exercise

--- workbook1d/SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
+++ workbook1d/SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -10,25 +10,43 @@ public class SandwichShop {
         //get size
         System.out.print("""
                 \nWhich size would you like? 🥪
-                   1. Regular: base price $5.45
-                   2. Large: base price $8.95
+                    1. Regular: base price $5.45
+                    2. Large: base price $8.95
                 Enter 1 or 2:\s""");
         int size = scnr.nextInt();
 
-        //determine base price
-        double price = 5.45;
-        if (size == 2) {
-            price = 8.95;
-        }
-        else if (size != 1) {
+        if (size != 1 && size != 2) {
             System.out.println("Invalid option. Defaulting to regular size.");
+            size = 1;
         }
 
-        //get age
+        //loaded or not
+        System.out.print("""
+                \nWould you like the sandwich loaded?
+                    Regular: +$1.00
+                    Large: +$1.75
+                Enter "true" or "false" exactly:\s""");
+        boolean isLoaded = scnr.nextBoolean();
+
+        //determine price
+        double price = 0;
+        if (size == 1) {
+            price = 5.45;
+            if (isLoaded) {
+                price += 1;
+            }
+        }
+        else {
+            price = 8.95;
+            if (isLoaded) {
+                price += 1.75;
+            }
+        }
+
+        //get age & discount if applicable
         System.out.print("\nHow old are you?: ");
         int age = scnr.nextInt();
 
-        //discount if applicable
         if (age < 18) {
             System.out.println("Congrats! You're eligible for a student discount.");
             price -= price * 0.10;
