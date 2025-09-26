10a53e994f63defa43ebec15dac2133783474cd0
Jasmine <jasminetntu@gmail.com>
2025-09-26 10:48:27 -0700

    start workshop 1w

---
+++ workshops/financial-calculators/src/main/java/com/pluralsight/FinancialCalculators.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FinancialCalculators {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Welcome to our Financial Calculators!");
+
+
+
+
+        scnr.close();
+    }
+
+    public void printMenu() {
+
+    }
+
+    public void mortgageCalculator() {
+
+    }
+
+    public void futureValueCalculator() {
+
+    }
+
+    public void presentValueCalculator() {
+
+    }
+}
