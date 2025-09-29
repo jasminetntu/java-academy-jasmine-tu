e114601d2b5775e1a98dd3360d23556c28c9e014
Jasmine <jasminetntu@gmail.com>
2025-09-29 11:40:12 -0700

    finish FullNameParser.java

--- workbook2a/FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
+++ workbook2a/FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
@@ -7,6 +7,21 @@ public class FullNameParser {
         Scanner scnr = new Scanner(System.in);
         System.out.println("*** Full Name Parser ***");
 
+        //get input
+        System.out.print("Please enter your full name: ");
+        String fullName = scnr.nextLine().trim();
 
+        //get first/middle/last
+        String[] name_arr = fullName.split(" ");
+
+        //print result
+        System.out.println("\nFirst name: " + name_arr[0]);
+
+        System.out.print("Middle name: ");
+        for (int i = 1; i < name_arr.length - 1; ++i) {
+            System.out.print(name_arr[i] + " ");
+        }
+
+        System.out.println("\nLast name: " + name_arr[name_arr.length - 1]);
     }
 }
