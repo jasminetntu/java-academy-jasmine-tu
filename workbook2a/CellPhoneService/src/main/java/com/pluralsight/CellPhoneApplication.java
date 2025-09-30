f2fc862ac5824dbfd5fc62f3ea29ea79313fa8f8
Jasmine <jasminetntu@gmail.com>
2025-09-30 15:16:26 -0700

    implement phone application

--- workbook2a/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
+++ workbook2a/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,7 +1,42 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("*** Cell Phone Service ***");
 
+        CellPhone phone = new CellPhone();
+
+        //get inputs & call setters
+        System.out.print("What is the serial number?: ");
+        phone.setSerialNumber(scnr.nextInt());
+        scnr.nextLine(); //consume leftover \n
+
+        System.out.print("What model is the phone?: ");
+        phone.setModel(scnr.nextLine().trim());
+
+        System.out.print("Who is the carrier?: ");
+        phone.setCarrier(scnr.nextLine().trim());
+
+        System.out.print("What is the phone number?: ");
+        phone.setPhoneNumber(scnr.nextLine().trim());
+
+        System.out.print("Who is the owner of the phone?: ");
+        phone.setOwner(scnr.nextLine().trim());
+
+        //print results w/ getters
+        System.out.printf("""
+                \nYour Phone:
+                Serial Number: %d
+                Model: %s
+                Carrier: %s
+                Phone Number: %s
+                Owner: %s
+                """, phone.getSerialNumber(), phone.getModel(), phone.getCarrier(),
+                phone.getPhoneNumber(), phone.getOwner());
+
+        scnr.close();
     }
 }
