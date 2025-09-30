0fef99c237d509e4e18779cf9fadb7118d1b35dc
Jasmine <jasminetntu@gmail.com>
2025-09-30 09:08:25 -0700

    java-loop exercises

--- class-demos/loop-practice/src/main/java/com/pluralsight/LoopPractice.java
+++ class-demos/loop-practice/src/main/java/com/pluralsight/LoopPractice.java
@@ -1,7 +1,10 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class LoopPractice {
     public static void main(String[] args) {
+        // *** class exercises ***
         //exercise 1
         System.out.println("Exercise 1:");
         for (int i = 1; i < 11; i++) {
@@ -18,6 +21,41 @@ public class LoopPractice {
                 System.out.println(i + " more days left of Year Up Code academy.");
             }
         }
+
+        // *** java-loops exercises ***
+        //exercise 1 - even nums
+        System.out.println("For Loop: Counting from 1 to 5");
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Even iteration " + i);
+            }
+        }
+
+        //exercise 2 - countdown timer
+        System.out.println("\nWhile Loop: Counting down from 5");
+        int j = 5;
+        while (j > 0) {
+            System.out.println("Countdown: " + j + " s");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            j--;
+        }
+
+        //exercise 3 - input until exit
+        System.out.println("\nDo...While Loop: Run at least once");
+
+        Scanner scnr = new Scanner(System.in);
+        String input;
+        do {
+            System.out.print("Enter some input or type 'exit': ");
+            input = scnr.nextLine();
+        } while (!input.equalsIgnoreCase("exit"));
+
 
     }
 }
