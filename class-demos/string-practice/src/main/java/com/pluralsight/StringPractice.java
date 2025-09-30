42f2ae6ece229b1b3dd6217a37a0a5ec91850b38
Jasmine <jasminetntu@gmail.com>
2025-09-30 11:26:29 -0700

    login checker methods

--- class-demos/string-practice/src/main/java/com/pluralsight/StringPractice.java
+++ class-demos/string-practice/src/main/java/com/pluralsight/StringPractice.java
@@ -11,15 +11,28 @@ public class StringPractice {
         //assume case doesn't matter
 
         ValidateEmail(email);
+        LoginChecker(scnr);
     }
 
     public static void ValidateEmail(String email) {
         boolean isValid = email.endsWith("@yearup.org");
         if (isValid) {
             System.out.println(email + " is valid.");
-        }
-        else {
+        } else {
             System.out.println(email + " is invalid.");
         }
+    }
+
+    private static void LoginChecker(Scanner scnr) {
+        System.out.println("What is your password");
+        String password = scnr.nextLine().trim();
+        String correctPassword = "1234";
+
+        if (password.equals(correctPassword)) {
+            System.out.println("That is correct, you are logged in");
+        } else {
+            System.out.println("That is incorrect, try again");
+        }
+
     }
 }
