b52526b18cbbddb54c2bdd9849b65c0447309e7a
Jasmine <jasminetntu@gmail.com>
2025-10-02 11:43:34 -0700

    create PalindromeProduct.java exercise

--- workbook2b/CarInventory/src/main/java/com/pluralsight/CarInventory.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight;
-
-public class CarInventory {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ workbook2b/PalindromeProduct/src/main/java/com/pluralsight/PalindromeProduct.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PalindromeProduct {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+    }
+}
