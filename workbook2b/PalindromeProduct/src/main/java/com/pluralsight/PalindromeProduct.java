c0f71faa345db5beec023f2effaf6bb4e0467b70
Jasmine <jasminetntu@gmail.com>
2025-10-02 12:13:51 -0700

    finish PalindromeProduct.java exercise

--- workbook2b/PalindromeProduct/src/main/java/com/pluralsight/PalindromeProduct.java
+++ workbook2b/PalindromeProduct/src/main/java/com/pluralsight/PalindromeProduct.java
@@ -1,10 +1,33 @@
 package com.pluralsight;
 
-import java.util.Scanner;
-
 public class PalindromeProduct {
-
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
+        System.out.println("*** Palindrome Product ***");
+
+        int maxPalindrome = 0;
+        String product;
+        StringBuilder sbReversed = new StringBuilder();
+
+        System.out.println("Let's find the largest palindrome made from the product of two 3-digit numbers!");
+
+        //find max palindrome
+        for (int i = 1; i < 1000; ++i) {
+            for (int j = 1; j < 1000; ++j) {
+                //get product & reversed product
+                product = String.valueOf(i * j);
+                sbReversed.append(product).reverse();
+
+                //check if palindrome & update if new max
+                if (product.contentEquals(sbReversed) && Integer.parseInt(product) > maxPalindrome) {
+                    maxPalindrome = Integer.parseInt(product);
+                }
+
+                //clear sb
+                sbReversed.setLength(0);
+            }
+        }
+
+        //print max
+        System.out.println("Max palindrome: " + maxPalindrome);
     }
 }
