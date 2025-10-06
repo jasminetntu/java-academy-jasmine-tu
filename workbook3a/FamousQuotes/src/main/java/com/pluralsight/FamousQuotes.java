e1b0f03bcbed99dc65ff936da8d061af9077e899
Jasmine <jasminetntu@gmail.com>
2025-10-06 09:42:16 -0700

    set up famous quotes exercise

---
+++ workbook3a/FamousQuotes/src/main/java/com/pluralsight/FamousQuotes.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FamousQuotes {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("*** Famous Quotes ***");
+    }
+}
