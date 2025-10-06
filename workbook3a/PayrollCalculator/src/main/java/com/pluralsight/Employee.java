d83eaeffc9b2ccf9c933b16db43d0cf7d3bb7e12
Jasmine <jasminetntu@gmail.com>
2025-10-06 11:12:25 -0700

    set up payrollcalculator

---
+++ workbook3a/PayrollCalculator/src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,50 @@
+package com.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    // *** Constructors ***
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    // *** Getters ***
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    // *** Setters ***
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+}
