891b7b8c6080b00230a44e0b6824a89b83f5941a
Jasmine <jasminetntu@gmail.com>
2025-10-07 14:54:52 -0700

    finish FormatDates.java (w/ challenge exercise)

--- workbook3a/FormatDates/src/main/java/com/pluralsight/FormatDates.java
+++ workbook3a/FormatDates/src/main/java/com/pluralsight/FormatDates.java
@@ -9,31 +9,31 @@ public class FormatDates {
 
         //get current date & time
         LocalDate currDate = LocalDate.now();
-        LocalTime currTime = LocalTime.now();
+        LocalDateTime currDateAndTime = LocalDateTime.now();
 
         //display in 4 ways
+        //09/05/2021
         formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         System.out.println(formatter.format(currDate));
 
+        //2021-09-05
         formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         System.out.println(formatter.format(currDate));
 
+        //September 5, 2021
         formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
         System.out.println(formatter.format(currDate));
 
-        formatter = DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy");
-        System.out.println(formatter.format(currDate) + " " + currTime);
+        //Sunday, Sep 5, 2021 10:02 (GMT)
+        formatter = DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy HH:mm");
+        System.out.println(formatter.format(currDateAndTime));
 
+        //5:02 on 05-Sep-2021 (PDT/PST)
+        //get time zone
+        ZoneId zoneId = ZoneId.of("America/Los_Angeles");
+        ZonedDateTime zonedDateTime = currDateAndTime.atZone(zoneId);
 
-
-
-
-
-        //09/05/2021
-        //2021-09-05
-        //September 5, 2021
-        //Sunday, Sep 5, 2021 10:02 ß display in GMT time
-        //CHALLENGE
-        //5:02 on 05-Sep-2021 ß display in your local time zone
+        formatter = DateTimeFormatter.ofPattern("h:mm a zzz, dd-MMM-yyyy");
+        System.out.println(formatter.format(zonedDateTime));
     }
 }
