f1fb3d3fce1a4722544f450be2bb02d6261aad54
Jasmine <jasminetntu@gmail.com>
2025-10-07 20:04:02 -0700

    bonus: read from inventory.csv

--- workbook3a/SearchInventory/src/main/java/com/pluralsight/SearchInventory.java
+++ workbook3a/SearchInventory/src/main/java/com/pluralsight/SearchInventory.java
@@ -2,30 +2,53 @@ package com.pluralsight;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*;
+import java.net.URL;
 
 public class SearchInventory {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArrayList<Product> inventory = getInventory();
         Scanner scnr = new Scanner(System.in);
 
         System.out.println("We carry the following inventory: ");
         for (int i = 0; i < inventory.size(); i++) {
             Product p = inventory.get(i);
-            System.out.printf("id: %d %s - Price: $%.2f\n",
+            System.out.printf("id: %d, %s - Price: $%.2f\n",
                     p.getId(), p.getName(), p.getPrice());
         }
 
         scnr.close();
     }
 
-    public static ArrayList<Product> getInventory() {
-        ArrayList<Product> inventory = new ArrayList<Product>();
-        inventory.add(new Product(1, "Apple", 1.99));
-        inventory.add(new Product(2, "Banana", 0.99));
-        inventory.add(new Product(3, "Grapes", 2.99));
-        inventory.add(new Product(4, "Cuties", 5.99));
-        inventory.add(new Product(5, "24K Golden Labubu", 99.99));
+    public static ArrayList<Product> getInventory() throws IOException {
+        ArrayList<Product> inventory = new ArrayList<>();
 
+        URL resource = SearchInventory.class.getClassLoader().getResource("inventory.csv");
+
+        if (resource == null) {
+            System.out.println("Error: File not found.");
+            return inventory; //returns empty inventory
+        }
+
+        File file = new File(resource.getFile());
+        String currLine;
+        String[] tempArr;
+
+        try (BufferedReader bufReader = new BufferedReader(new FileReader(file))) {
+            while ((currLine = bufReader.readLine()) != null) {
+                tempArr = currLine.split("\\|");
+
+                // create new product (id, name, price) -> add to inventory
+                inventory.add(new Product(Integer.parseInt(tempArr[0]), tempArr[1], Double.parseDouble(tempArr[2])));
+            }
+        }
+
+//        5 items for testing
+//        inventory.add(new Product(1, "Apple", 1.99));
+//        inventory.add(new Product(2, "Banana", 0.99));
+//        inventory.add(new Product(3, "Grapes", 2.99));
+//        inventory.add(new Product(4, "Cuties", 5.99));
+//        inventory.add(new Product(5, "24K Golden Labubu", 99.99));
         return inventory;
     }
 }
