44dfce5c655718facd54c374aa107eaef3fbba52
Jasmine <jasminetntu@gmail.com>
2025-10-23 22:52:02 -0700

    car dealership phase 1: class structure

---
+++ workshops/4w-car-dealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class DealershipFileManager {
+    public Dealership getDealership() {
+        return null;
+    }
+
+    public void saveDealership(Dealership dealership) {
+
+    }
+}
---
+++ workshops/4w-car-dealership/src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,44 @@
+package com.pluralsight;
+
+public class UserInterface {
+    public void display() {
+
+    }
+
+    public void processGetByPriceRequest() {
+
+    }
+
+    public void processGetByMakeModelRequest() {
+
+    }
+
+    public void processGetByYearRequest() {
+
+    }
+
+    public void processGetByColorRequest() {
+
+    }
+
+    public void processGetByMileageRequest() {
+
+    }
+
+    public void processGetByVehicleTypeRequest() {
+
+    }
+
+    public void processGetAllVehiclesRequest() {
+
+    }
+
+    public void processAddVehicleRequest() {
+
+    }
+
+    public void processRemoveVehicleRequest() {
+
+    }
+
+}
