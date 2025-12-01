38b068fd8dfe2e78bf8002f61fbdbaee4de2b1ac
Jasmine <jasminetntu@gmail.com>
2025-12-01 10:55:35 -0800

    complete NorthwindTraders exercise

---
+++ workbook8c/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,36 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // load MySQL driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // open connection
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/northwind",
+                "root",
+                "yearup25");
+
+        // create statement
+        Statement statement = connection.createStatement();
+
+        // define query
+        String query = "SELECT * FROM northwind.products";
+
+        // execute query
+        ResultSet resultSet = statement.executeQuery(query);
+
+        // process results
+        System.out.println("*** Northwind Products (ID & Name) ***");
+        while (resultSet.next()) {
+            String productName = resultSet.getString("ProductName");
+            int productId = resultSet.getInt("ProductId");
+            System.out.println("[" + productId + "] " + productName);
+        }
+
+        // close connection
+        connection.close();
+    }
+}
