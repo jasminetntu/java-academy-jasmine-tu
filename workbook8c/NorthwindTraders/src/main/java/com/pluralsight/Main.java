85ecc17e8321f02d5da29215f1ca610acb797d3f
Jasmine <jasminetntu@gmail.com>
2025-12-07 13:31:03 -0800

    complete NorthwindTraders exercise

--- workbook8c/NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ workbook8c/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -4,34 +4,46 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // exit if username & password aren't provided
+        if (args.length != 2) {
+            System.out.println("Application needs two arguments to run: " +
+            "java com.pluralsight.UsingDriverManager <username> <password>");
+            System.exit(1);
+        }
+        String username = args[0]; // otherwise, get CLI args from config
+        String password = args[1];
+
         // load MySQL driver
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         // open connection
-        // DriverManager = classic/older way
-        Connection connection = DriverManager.getConnection(
+        Connection connection = DriverManager.getConnection( // DriverManager = classic/older way
                 "jdbc:mysql://localhost:3306/northwind",
-                "root",
-                "insertpasswordhere");
+                username, password);
 
         // create statement
-        Statement statement = connection.createStatement();
-
-        // define query
-        String query = "SELECT * FROM northwind.products";
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT productId, productName, unitPrice, unitsInStock FROM northwind.products " +
+                        "ORDER BY productId");
 
         // execute query
-        ResultSet resultSet = statement.executeQuery(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
 
         // process results
-        System.out.println("*** Northwind Products (ID & Name) ***");
+        System.out.println("*** Northwind Products ***");
+        System.out.println(String.format("%-4s %-40s %-7s %-6s%n", "Id", "Name", "Price", "Stock") +
+                "---- ---------------------------------------- ------- ------");
         while (resultSet.next()) {
-            String productName = resultSet.getString("ProductName");
-            int productId = resultSet.getInt("ProductId");
-            System.out.println("[" + productId + "] " + productName);
+            int productId = resultSet.getInt("productId");
+            String productName = resultSet.getString("productName");
+            String unitPrice = String.format("%.2f", resultSet.getDouble("unitPrice"));
+            int unitsInStock = resultSet.getInt("unitsInStock");
+            System.out.printf("%-4s %-40s %-7s %-6d%n", productId, productName, unitPrice, unitsInStock);
         }
 
-        // close connection
+        // close the resources
+        resultSet.close();
+        preparedStatement.close();
         connection.close();
     }
 }
