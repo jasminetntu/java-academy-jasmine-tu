2e20f4803db07d3d6f6d72a92387f75fe515bcc4
Jasmine <jasminetntu@gmail.com>
2025-12-07 15:09:00 -0800

    complete NorthwindTraders exercise 5 (refactor with datasource)

--- workbook8c/NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ workbook8c/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,9 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+import javax.xml.transform.Result;
 import java.sql.*;
 import java.util.Scanner;
 
@@ -14,9 +18,11 @@ public class Main {
         String username = args[0]; // otherwise, get CLI args from config
         String password = args[1];
 
-        // load MySQL driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
+        // create data source
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("com.mysql.cj.jdbc.Driver");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
 
         boolean running = true;
         try (Scanner scnr = new Scanner(System.in)) {
@@ -32,11 +38,11 @@ public class Main {
                 String choice = scnr.nextLine().trim().toLowerCase();
 
                 switch (choice) {
-                    case "1" -> main.displayProducts(username, password);
-                    case "2" -> main.displayCustomers(username, password);
+                    case "1" -> main.displayProducts(dataSource);
+                    case "2" -> main.displayCustomers(dataSource);
                     case "3" -> {
-                        main.displayCategories(username, password);
-                        main.displayProductsFromCategory(username, password, scnr);
+                        main.displayCategories(dataSource);
+                        main.displayProductsFromCategory(dataSource, scnr);
                     }
                     case "0" -> {
                         System.out.println("Goodbye!");
@@ -48,140 +54,70 @@ public class Main {
         }
     }
 
-    private void displayProducts(String username, String password) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
-        try {
-            // open connection
-            connection = DriverManager.getConnection( // DriverManager = classic/older way
-                    "jdbc:mysql://localhost:3306/northwind",
-                    username, password);
-
-            // create statement
-            preparedStatement = connection.prepareStatement(
-                    "SELECT productId, productName, unitPrice, unitsInStock FROM northwind.products " +
-                            "ORDER BY productId");
-
+    private void displayProducts(BasicDataSource dataSource) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "SELECT productId, productName, unitPrice, unitsInStock FROM northwind.products " +
+                     "ORDER BY productId")) {
             // execute query
-            resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            // process results
-            System.out.println("\n*** Northwind Products ***");
-            System.out.printf("%-4s %-40s %-7s %-6s%n", "Id", "Name", "Price", "Stock");
-            System.out.println("---- ---------------------------------------- ------- ------");
+                // process results
+                System.out.println("\n*** Northwind Products ***");
+                System.out.printf("%-4s %-40s %-7s %-6s%n", "Id", "Name", "Price", "Stock");
+                System.out.println("---- ---------------------------------------- ------- ------");
 
-            while (resultSet.next()) {
-                int productId = resultSet.getInt("productId");
-                String productName = resultSet.getString("productName");
-                String unitPrice = String.format("%.2f", resultSet.getDouble("unitPrice"));
-                int unitsInStock = resultSet.getInt("unitsInStock");
-                System.out.printf("%-4s %-40s %-7s %-6d%n", productId, productName, unitPrice, unitsInStock);
+                while (resultSet.next()) {
+                    int productId = resultSet.getInt("productId");
+                    String productName = resultSet.getString("productName");
+                    String unitPrice = String.format("%.2f", resultSet.getDouble("unitPrice"));
+                    int unitsInStock = resultSet.getInt("unitsInStock");
+                    System.out.printf("%-4s %-40s %-7s %-6d%n", productId, productName, unitPrice, unitsInStock);
+                }
+                System.out.println("---- ---------------------------------------- ------- ------\n");
             }
-            System.out.println("---- ---------------------------------------- ------- ------\n");
         }
         catch (SQLException e) {
             System.out.println("Something went wrong while working with database.");
             e.printStackTrace();
         }
-        finally {
-            // close the resources
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 
-    private void displayCustomers(String username, String password) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
+    private void displayCustomers(BasicDataSource dataSource) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "SELECT contactName, companyName, city, country, phone FROM northwind.customers " +
+                     "ORDER BY country")) {
 
-        try {
-            // open connection
-            connection = DriverManager.getConnection( // DriverManager = classic/older way
-                    "jdbc:mysql://localhost:3306/northwind",
-                    username, password);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                // process results
+                System.out.println("\n*** Northwind Customers ***");
+                System.out.printf("%-30s %-40s %-20s %-15s %-15s%n",
+                        "Contact Name", "Company Name", "City", "Country", "Phone");
+                System.out.println("------------------------------ ---------------------------------------- -------------------- --------------- ---------------");
 
-            // create statement
-            preparedStatement = connection.prepareStatement(
-                    "SELECT contactName, companyName, city, country, phone FROM northwind.customers " +
-                            "ORDER BY country");
-
-            // execute query
-            resultSet = preparedStatement.executeQuery();
-
-            // process results
-            System.out.println("\n*** Northwind Customers ***");
-            System.out.printf("%-30s %-40s %-20s %-15s %-15s%n",
-                    "Contact Name", "Company Name", "City", "Country", "Phone");
-            System.out.println("------------------------------ ---------------------------------------- -------------------- --------------- ---------------");
-
-            while (resultSet.next()) {
-                String contactName = resultSet.getString("contactName");
-                String companyName = resultSet.getString("companyName");
-                String city = resultSet.getString("city");
-                String country = resultSet.getString("country");
-                String phone = resultSet.getString("phone");
-                System.out.printf("%-30s %-40s %-20s %-15s %-15s%n", contactName, companyName, city, country, phone);
+                while (resultSet.next()) {
+                    String contactName = resultSet.getString("contactName");
+                    String companyName = resultSet.getString("companyName");
+                    String city = resultSet.getString("city");
+                    String country = resultSet.getString("country");
+                    String phone = resultSet.getString("phone");
+                    System.out.printf("%-30s %-40s %-20s %-15s %-15s%n", contactName, companyName, city, country, phone);
+                }
+                System.out.println("------------------------------ ---------------------------------------- -------------------- --------------- ---------------\n");
             }
-            System.out.println("------------------------------ ---------------------------------------- -------------------- --------------- ---------------\n");
         }
         catch (SQLException e) {
             System.out.println("Something went wrong while working with database.");
             e.printStackTrace();
         }
-        finally {
-            // close the resources
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 
-    private void displayCategories(String username, String password) {
-        try (Connection connection = DriverManager.getConnection( // DriverManager = classic/older way
-                "jdbc:mysql://localhost:3306/northwind", username, password);
+    private void displayCategories(BasicDataSource dataSource) {
+        try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(
                      "SELECT categoryId, categoryName FROM northwind.categories " +
-                     "ORDER BY categoryId")) {
+                             "ORDER BY categoryId")) {
 
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 System.out.println("\n*** Northwind Categories ***");
@@ -194,9 +130,6 @@ public class Main {
                     System.out.printf("%-5s %-15s%n", categoryId, categoryName);
                 }
                 System.out.println("----- ---------------\n");
-            } catch (SQLException e) {
-                System.out.println("Something went wrong while reading results.");
-                e.printStackTrace();
             }
         }
         catch (SQLException e) {
@@ -205,13 +138,12 @@ public class Main {
         }
     }
 
-    private void displayProductsFromCategory(String username, String password, Scanner scnr) {
-        try (Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/northwind", username, password);
+    private void displayProductsFromCategory(BasicDataSource dataSource, Scanner scnr) {
+        try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(
-                "SELECT productId, productName, unitPrice, unitsInStock FROM northwind.products " +
-                        "WHERE categoryId = ? ORDER BY productId"
-        )) {
+                     "SELECT productId, productName, unitPrice, unitsInStock FROM northwind.products " +
+                             "WHERE categoryId = ? ORDER BY productId"
+             )) {
             System.out.print("Enter category ID to search products by: ");
             int categoryId = Integer.parseInt(scnr.nextLine());
             preparedStatement.setInt(1, categoryId);
@@ -229,9 +161,6 @@ public class Main {
                     System.out.printf("%-4s %-40s %-7s %-6d%n", productId, productName, unitPrice, unitsInStock);
                 }
                 System.out.println("---- ---------------------------------------- ------- ------\n");
-            } catch (SQLException e) {
-                System.out.println("Something went wrong while reading results.");
-                e.printStackTrace();
             }
         }
         catch (SQLException e) {
